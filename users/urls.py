3651c3cb3aa54d0bf50834a48436496219757bd2
ARFCAC <76755363+ARFCAC@users.noreply.github.com>
2021-12-08 18:09:00 +0900

    一旦セーブ(ログイン関係整理)

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.urls import path
 from . import views
 from.apps import UserConfig
 
@@ -8,13 +8,13 @@ APP_LABEL_USER = UserConfig.name
 
 urlpatterns = [
     # ホーム
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
     path('', views.IndexView.as_view(), name='index'),
     path('howto/', views.HowtoView.as_view(), name='howto'),
-
-    path('', include('social_django.urls', namespace='social')),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 
     path("create/", views.UserInfoCreateView.as_view(), name="create"),
     path("list/", views.UserInfoListView.as_view(), name="list"),
     path("update/<int:pk>/", views.UserInfoUpdateView.as_view(), name="update"),
+    path("delete/<int:pk>/", views.UserInfoDeleteView.as_view(), name="delete"),
 ]
